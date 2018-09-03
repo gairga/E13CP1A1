9d358eb5e6b65e7a9480fc7f52fbb4d1a01e33be
Gabriela <gabrielailarreta@gmail.com>
2018-09-03 20:22:28 -0300

    Validaciones

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,6 @@
 class Sale < ApplicationRecord
 	validates :cod, uniqueness: true
+	validates :detail, presence: true
+	validates :category, inclusion: { in: %w(uno dos tres cuatro cinco), message: "%{value} is not a valid size" }
+  	validates :points, numericality: true
 end
