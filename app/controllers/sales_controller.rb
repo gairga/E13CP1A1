82237f01b437a2987cd5e62986d83556111f60c1
Gabriela <gabrielailarreta@gmail.com>
2018-09-03 21:44:24 -0300

    Correcciones Rutas

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,22 @@
 class SalesController < ApplicationController
+
+  def done
+    @sales = Sale.all
+  end
+
   def new
+    @sale = Sale.new
+  end
+
+  def create
+  #	byebug
+	@sale = Sale.new(sale_params)
+	@sale.save
+    redirect_to sales_path
+  end 
+
+  private
+  def sale_params
+    params.require(:sale).permit(:cod, :category, :detail, :value, :discount, :tax, :total)
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,5 +2,5 @@ class Sale < ApplicationRecord
 	validates :cod, uniqueness: true
 	validates :detail, presence: true
 	validates :category, inclusion: { in: %w(uno dos tres cuatro cinco), message: "%{value} is not a valid size" }
-  	validates :points, numericality: true
+  	validates :value, numericality: true
 end
